20cb3d09d6ea294e243a0b7b5f6c3c898414550e
mugi <munkhuu1233@gmail.com>
2021-02-24 18:43:54 -0500

    updated

--- routes/api/foodItems.js
+++ routes/api/foodItems.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 const foodItemsController = require("../../controllers/foodItemsController");
 
-// Matches with "/api/restaurants"
+
 router
   .route("/")
   .get(foodItemsController.findAll)
   .post(foodItemsController.create);
 
-// Matches with "/api/books/:id"
+
 router
   .route("/:id")
   .get(foodItemsController.findById)
