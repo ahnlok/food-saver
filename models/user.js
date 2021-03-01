702d7277b8ff319ef60cc20de34da6e894130ab7
Sungpil An <ansungpil1@gmail.com>
2021-03-01 14:45:51 -0500

    Complete Authentication process

--- models/user.js
+++ models/user.js
@@ -1,30 +1,11 @@
 
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-// const userSchema = new Schema({
-//   email: {
-//     type: String,
-//     trim: true,
-//     required: true,
-//     unique: true,
-//   },
-//   password: {
-//     type: String,
-//     trim: true,
-//   },
-//   food: [{
-//     type: Schema.Types.ObjectId, 
-//     ref: 'Food'
-//   }]
-// });
-
-// userSchema.pre("save", function (next) {
-//   this.email = this.email.toLowerCase();
-//   next();
-// });
-
-// const User = mongoose.model("User", userSchema);
-
-// module.exports = User;
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+});
+const User = mongoose.model("User", userSchema);
+module.exports = User;
