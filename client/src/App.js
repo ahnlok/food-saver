b0083c8792836ffbd838c03dab0187768cbd2040
mya todd <mya.todd428376@gmail.com>
2021-03-03 11:28:46 -0500

    trying to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,5 @@
-import React, { useState } from "react";
-
-import LoginInView from "./components/views/LoginInView";
-import AddItemView from "./components/views/AddItemView";
-
-import IndexView from "./components/views/IndexView";
-import NotFoundView from "./components/views/NotFoundView";
-import UnauthorizedView from "./components/views/UnauthorizedView";
-import SignupView from "./components/views/SignupView";
-import Items from "./components/Items"
-import CredentialsContext from "./util/Test"
-
-import Signup2 from './components/views/Signup2'
-import "./components/scripts";
-import "./components/styles";
-import "./components/views";
-
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import AddItemForm from "./forms/AddItemForm";
-
-
-
 const App = () => {
-  const credentialState = useState(null);
+  const credentialsState = useState(null);
   const [id, setId] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -35,22 +13,14 @@ const App = () => {
       <CredentialsContext.Provider value={{id, username, password, setCredential}}>
         <Router>
           <Switch>
-            <Route exact path="/">
-              <IndexView />
+            <Route exact path="/"><Welcome />
             </Route>
             <Route exact path="/register">
-              <Register js/>
+              <Register />
             </Route>
-            <Route exact path="/Login">
-              <Login js />
+            <Route exact path="/login">
+              <Login />
             </Route>
-            <Route exact path="/AddItem">
-              <AddItemForm />
-            </Route>
-            <Route exact path="/Sign up">
-              <SignupView />
-            </Route>
-            
             <Route exact path="/items">
               <Items />
             </Route>
@@ -58,6 +28,6 @@ const App = () => {
         </Router>
       </CredentialsContext.Provider>
     </div>
-  );
-};
+  )
+}
 export default App;
